a96680369cbb45c6cd6116952cbb3298fc05d062
Mukhammadkarim's MacBook Pro <macbook@Muhammadkarims-MacBook-Air.local>
2023-09-30 23:46:57 +0500

    added project

---
+++ Table App/app.js
@@ -0,0 +1,34 @@
+const btn = document.querySelector('.sub-btn');
+const tableBody = document.querySelector('.tabBody');
+const rContainer = document.querySelector('.row-container');
+
+//Events
+const rowContainer = `<tr class="row-container">
+          <td><input type="text" class="inp" /></td>
+          <td><input type="text" class="inp" /></td>
+          <td><input type="text" class="inp" /></td>
+          <td><input type="text" class="inp" /></td>
+          <td><input type="text" class="inp" /></td>
+</tr>`;
+
+tableBody.innerHTML = rowContainer;
+btn.addEventListener('click', addRow)
+
+let num = 0;
+
+function addRow() {
+	let lastRow = tableBody.lastElementChild;
+	const inpValue = lastRow.getElementsByTagName('input');
+	for (let i = 0; i < inpValue.length; i++) {
+		if (!inpValue[i].value) {
+			num++;
+			alert("Please do'nt skip any input!");
+			break;
+		}
+	}
+	if (num === 0) {
+		tableBody.innerHTML += rowContainer;
+
+	}
+	num = 0
+}
