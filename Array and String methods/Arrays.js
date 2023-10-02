5405bb87dabab97ffc730fd3194bacb97fe476be
Mukhammadkarim's MacBook Pro <macbook@Muhammadkarims-MacBook-Air.local>
2023-10-02 22:40:20 +0500

    added arr file

---
+++ Array and String methods/Arrays.js
@@ -0,0 +1,20 @@
+const arr1 = ['a', 'b', 'c', 'd', 'e', 'f'];
+const arr2 = [10, 15, 20, 25, 30];
+
+//// [ITERATOR]
+// const arr = ['a', 'b', 'c'];
+// const iterator1 = arr[Symbol.iterator]();
+
+// console.log(iterator1.next().value);
+// console.log(iterator1.next().value);
+// console.log(iterator1.next().value);
+// console.log(iterator1.next().value);
+
+// for (const v of arr) {
+// 	console.log(v);
+//  }
+
+//// AT
+// console.log(arr1.at(2));
+
+//// CONCAT
